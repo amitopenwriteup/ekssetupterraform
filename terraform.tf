e86f13513608fc3cd17bed5c06da68e23aa073e2
amit <amit@ow.com>
2024-05-01 04:56:51 +0000

    adding with cloud option

--- terraform.tf
+++ terraform.tf
@@ -3,21 +3,11 @@
 
 terraform {
 
-  cloud {
-    workspaces {
-      name = "learn-terraform-eks"
-    }
-  }
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.7.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.5.1"
     }
 
     tls = {
